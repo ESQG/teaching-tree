d7e19240fa34689635273d084c64eec5a1f1ff08
ESQG <esqg@alumni.stanford.edu>
2017-04-01 13:00:11 -0700

    Revamped model decided to use it

--- model.py
+++ model.py
@@ -2,6 +2,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
+class NaturalLanguage(db.Model):
+    __tablename__ = "natural_languages"
+    language_code = db.Column(db.String(20), primary_key=True)
+    name = db.Column(db.String(100))
+    i_speak_this_language = db.Column(db.String(500))
+
 class Medium(db.Model):
     __tablename__ = "media"
     medium_code = db.Column(db.String(10), primary_key=True)
@@ -22,11 +29,6 @@ class AccessNeeds(db.Model):
     acccomodations = db.Column(db.Text)
 
 
-class NaturalLanguage(db.Model):
-    __tablename__ = "natural_languages"
-    language_code = db.Column(db.String(20), primary_key=True)
-    name = db.Column(db.String(100))
-
 
 class ComputerLanguage(db.Model):
     __tablename__ = "computer_languages"
@@ -40,19 +42,22 @@ class Resource(db.Model):
     __tablename__ = "resources"
 
     resource_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-
-    medium = db.Column(db.String(10), db.ForeignKey('media.medium_code'))
+    title = db.Column(db.String(100))
+    url = db.Column(db.Text)
+    # medium = db.Column(db.String(10), db.ForeignKey('media.medium_code'))
+    medium = db.Column(db.String(20))
     natural_language = db.Column(db.String(20), db.ForeignKey('natural_languages.language_code'))
     computer_language = db.Column(db.String(20), db.ForeignKey('computer_languages.language_code'))
     subject = db.Column(db.String(20), db.ForeignKey('subjects.subject_code'))
     audience = db.Column(db.String(20), db.ForeignKey('audiences.audience_code'))
+    notes = db.Column(db.Text)
 
-class AudienceLevel(db.Model):
+class Audience(db.Model):
     __tablename__ = "audiences"
 
     audience_code = db.Column(db.String(20), primary_key=True)
     name = db.Column(db.String(100))
-    rank = db.Column(db.Integer)
+    rank = db.Column(db.Integer, unique=True)
     notes = db.Column(db.Text)
 
 class User(db.Model):
@@ -81,7 +86,7 @@ def connect_to_db(app, db_uri='postgresql:///teaching-tree', echo=False):
     # Configure to use our database
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     app.config['SQLALCHEMY_ECHO'] = echo
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
     db.app = app
     db.init_app(app)
     print "Connected to db %s" % db_uri.split('/')[-1] # Text after the last '/'
