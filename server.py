7c416cd5cdb937e0cfb76fff7e64fed4094ca3cf
ESQG <esqg@alumni.stanford.edu>
2017-04-01 08:32:36 -0700

    put subjects on homepage

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 import os
 import sys
+import json
 
 SECRET_KEY = os.environ.get('APP_KEY', 'FOR_LOOP')
 
@@ -12,9 +13,33 @@ app.secret_key = SECRET_KEY
 def home_page():
     return render_template('index.html')
 
-@app.route("/about/")
+@app.route("/about")
 def about_us():
     return render_template('about_us.html')
+
+@app.route("/learn")
+def learn():
+    subject = request.args.get('subject').lower()   # Makes it lowercase
+
+    data = read_json_data()
+    if subject in data['subjects']:
+        return render_template('resources.html', subject=subject, resources=data["resources"].get(subject, []))
+    else:
+        return redirect("/suggest/"+subject)
+
+@app.route("/suggest/<subject>")
+def suggest(subject):
+    return render_template('new_subject.html', subject=subject)
+
+
+
+
+
+def read_json_data():
+    try:
+        raw_data = json.load(open("resources.json"))
+    except ValueError:
+        return {"resources": [], "subjects": []}
 
 
 if __name__ == '__main__':
