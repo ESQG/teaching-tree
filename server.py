5a7478e8cbab568a3527ff9313ff725e04ea37f7
ESQG <esqg@alumni.stanford.edu>
2017-04-01 13:00:39 -0700

    Route to add resources

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, redirect, request, jsonify
+from flask import Flask, render_template, redirect, request, jsonify, flash
 import os
 import sys
 import json
+
+import data_interface
+from model import connect_to_db
 
 SECRET_KEY = os.environ.get('APP_KEY', 'FOR_LOOP')
 
@@ -24,17 +27,46 @@ def learn():
     if not subject:
         subject = request.args.get('select-subject')
 
-    data = read_json_data()
-    if subject in data['subjects']:
-        return render_template('resources.html', subject=subject, resources=data["resources"].get(subject, []))
-    else:
-        return redirect("/suggest/"+subject)
+    if not subject:
+        flash("Had an error: please pick a subject.")
+        return redirect("/")
+
+    known_subject = data_interface.get_subject(subject)
+    if not known_subject:
+        return render_template("new_subject.html", subject=subject)
+
+    subject_code = known_subject.subject_code
+    resources = data_interface.get_resources(subject_code)
+    return render_template("resources.html", resources=resources, subject=subject)
 
 @app.route("/suggest/<subject>")
 def suggest(subject):
-    return render_template('new_subject.html', subject=subject)
+    languages = data_interface.get_languages()
+    audiences = data_interface.get_audiences()
+    resources = data_interface.get_resources(subject)
+    return render_template('new_subject.html', subject=subject, audiences=audiences, languages=languages)
 
 
+@app.route("/add-resource", methods=["POST"])
+def add_new_resource():
+    subject = request.form.get("subject")
+    resource_data = {
+    "subject": subject,
+    "reading-level": int(request.form.get("reading-level")),
+    "subject-detail": request.form.get("subject-detail"),
+    "notes": request.form.get("notes"),
+    "url": request.form.get("link"),
+    "language": request.form.get("language"),
+    "medium": request.form.get("medium"),
+    "title": request.form.get("title")
+    }
+
+    with open("form_submission.log", 'w') as outfile:
+        outfile.write(str(resource_data))
+
+    data_interface.add_resource(resource_data)
+    flash("Thank you for providing this resource in %s!" % subject)
+    return redirect("/")
 
 @app.route("/test.json")
 def show_data():
@@ -57,5 +89,6 @@ if __name__ == '__main__':
         app.debug = True
     else:
         app.debug = False
+    connect_to_db(app)
 
     app.run()
