a0b22dddc98bb5f71e1786e3da4b04aa47c40b8e
ESQG <esqg@alumni.stanford.edu>
2017-04-01 06:14:01 -0700

    made landing page, setup templates

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+import os
+import sys
+
+SECRET_KEY = os.environ.get('APP_KEY', 'FOR_LOOP')
+
+
+app = Flask(__name__)
+app.secret_key = SECRET_KEY
+
+@app.route("/")
+def home_page():
+    return render_template('index.html')
+
+
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) > 1 and sys.argv[1] == 'debug':
+        app.debug = True
+    else:
+        app.debug = False
+
+    app.run()
